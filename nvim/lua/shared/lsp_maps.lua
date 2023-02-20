f82b78cfef469b292ab7e543d080b84c10f09ccf
pavlo.skobnikov <pavlo.skobnikov@pm.bet>
2023-02-20 09:30:24 +0200

    Update lsp_maps

--- nvim/lua/shared/lsp_maps.lua
+++ nvim/lua/shared/lsp_maps.lua
@@ -11,12 +11,12 @@ local map_w_opts = utility_fns.create_mapping_fn_with_default_opts_and_desc(
 function M.on_startup()
   map_w_opts("n", "<space>Q", builtin.diagnostics, "Show Global Diagnostics")
 
-  map_w_opts("n", "gS", builtin.lsp_workspace_symbols, "Go to Workspace Symbols")
+  map_w_opts("n", "gw", builtin.lsp_workspace_symbols, "Go to Workspace Symbols")
 end
 
 -- Use an on_attach function to only map the following keys
 --  after the language server attaches to the current buffer
-function M.on_attach(client, bufnr)
+function M.on_attach(bufnr)
   local map_w_buf_opts = utility_fns.create_mapping_fn_with_default_opts_and_desc(
     { noremap = true, silent = true, buffer = bufnr })
 
@@ -30,25 +30,33 @@ function M.on_attach(client, bufnr)
 
   map_w_opts("n", "<space>q", function() builtin.diagnostics({ bufnr = 0 }) end, "Show Local Diagnostics")
 
-  map_w_opts("n", "gs", builtin.lsp_document_symbols, "Go to Buffer Symbols")
+  map_w_opts("n", "gl", builtin.lsp_document_symbols, "Go to Buffer Symbols")
 
   map_w_opts("n", "gI", builtin.lsp_incoming_calls, "Go to Incoming Calls")
-  map_w_opts("n", "gO", builtin.lsp_outgoing_calls, "Go to Outgoing Calls")
+  map_w_buf_opts("n", "gi", builtin.lsp_implementations, "Go to Implementation")
+
+  map_w_opts("n", "go", builtin.lsp_outgoing_calls, "Go to Outgoing Calls")
 
   map_w_buf_opts("n", "gD", vim.lsp.buf.declaration, "Go to Declaration")
   map_w_buf_opts("n", "gd", builtin.lsp_definitions, "Go to Definition")
+
   map_w_buf_opts("n", "K", vim.lsp.buf.hover, "Show Documentation")
-  map_w_buf_opts("n", "gi", builtin.lsp_implementations, "Go to Implementation")
-  map_w_buf_opts({ "n", "i" }, "<C-/>", vim.lsp.buf.signature_help, "Signature Help")
+
+  map_w_buf_opts({ "n", "i", "v" }, "<A-k>", vim.lsp.buf.signature_help, "Signature Help")
+
   map_w_buf_opts("n", "<space>wa", vim.lsp.buf.add_workspace_folder, "Add Folder to Workspace")
   map_w_buf_opts("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, "Remove Folder from Workspace")
   map_w_buf_opts("n", "<space>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, "List Workspace Folders")
-  map_w_buf_opts("n", "gT", builtin.lsp_type_definitions, "Jump to Type Definition for Symbol")
-  map_w_buf_opts("n", "<space>rn", vim.lsp.buf.rename, "Rename")
-  map_w_buf_opts("n", "<space>ra", vim.lsp.buf.code_action, "Show Code Actions")
-  map_w_buf_opts("n", "gR", builtin.lsp_references, "Go to References")
+
+  map_w_buf_opts("n", "gt", builtin.lsp_type_definitions, "Jump to Type Definition for Symbol")
+
+  map_w_buf_opts("n", "<space>n", vim.lsp.buf.rename, "Rename")
+  map_w_buf_opts("n", "<space>a", vim.lsp.buf.code_action, "Show Code Actions")
+
+  map_w_buf_opts("n", "g*", builtin.lsp_references, "Go to References")
+
   map_w_buf_opts({"n", "v"}, "<space>=", function() vim.lsp.buf.format { async = true } end, "Format Buffer")
 end
 
